54672a12012df4bb9a3599589c3e9475a29060b0
Kartik Awari <kartikawari123@gmail.com>
2023-04-24 12:10:46 +0530

    ApiIntegration Checking

--- xyz/src/App.js
+++ xyz/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
 function App() {
+
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=1000')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="posts-container">
+      {posts.map((post) => {
+        return (
+          <div className="post-card" key={post.id}>
+            <h2 className="post-title">{post.title}</h2>
+            <p className="post-body">{post.body}</p>
+            <div className="button">
+              <div className="delete-btn">Delete</div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
